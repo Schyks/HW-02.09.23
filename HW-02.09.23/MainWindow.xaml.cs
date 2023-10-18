ca2d171573c8d79f9cc197602b0bb782a5ff68db
Schyks <127341306+Schyks@users.noreply.github.com>
2023-10-18 22:52:49 +0300

    Добавьте файлы проекта.

---
+++ HW-02.09.23/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_02._09._23
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Image image)
+            {
+                ContextMenu contextMenu = image.ContextMenu;
+                if (contextMenu != null)
+                {
+                    contextMenu.IsOpen = true;
+                }
+            }
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Image image)
+            {
+                ContextMenu contextMenu = image.ContextMenu;
+                if (contextMenu != null)
+                {
+                    contextMenu.IsOpen = false;
+                }
+            }
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
